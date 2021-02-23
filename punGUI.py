9dddff818010bba5d2f5bc1cfc0a018b5b7928c9
EEGemen-98 <54004471+EEGemen-98@users.noreply.github.com>
2021-02-23 13:40:41 -0800

    Add files via upload

---
+++ punGUI.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import *
+from gpt3_access import getPrompt
+
+# Root GUI object
+root = tk.Tk()
+
+# Translates input text into pun version
+def generate():
+    originalText = str(input.get())
+    response = getPrompt(originalText)
+    print(response)
+    outPiece = []
+    
+    outPiece = response.splitlines()
+    out_text = str(outPiece[6])
+    
+
+    output.insert(1, out_text[22:-1])
+
+inputLabel = Label(root, text="Enter a sentence: ")
+inputLabel.grid(row=0, column=0)
+inputLabel.pack()
+
+# Input text box
+input = Entry(root, width=50)
+input.pack()
+
+# Generate pun button
+genButton = Button(root, text="Punify", command=generate)
+genButton.pack()
+
+# Output box
+output = Entry(root, width=50)
+output.pack()
+
+
+root.mainloop()
