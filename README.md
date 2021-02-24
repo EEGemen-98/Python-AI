0fbd581f008758d0d5fe89a0c498c6eaf585cf7a
EEGemen-98 <EEGemen-98@users.noreply.github.com>
2021-02-24 15:04:51 -0800

    updated punGUI

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
 # Python-AI
 Work in progress, no license
 
-Currently working on a pun generator powered by gpt-3 AI model (OpenAI). 
-I'm planning to do another python app that gives synergy recommendations for the game League of Legends after pun generator is finished and I receive the Riot API key.
+This will soon become a pun generator powered by AI, currently the test file is a basic user text interface with OpenAI API.
 
-I need to get approval by OpenAI before I can publish each program as an app online or license.
-These programs don't work without an API key which is not included.
+I need to get approval by OpenAI before I can publish this as an app online or license.
 
-**punGUI.py log:**
- - (2/23/2021)
- - (Changes) created functional GUI that appropiately interacts with gpt-3
- - (Needs Work) GUI needs an organized layout and more labels
- 
- - (2/24/2021)
- - (Changes) organized layout a little better and improved AI interaction
- - (Needs Work) Polish GUI and handle empty text input case
+This program doesn't work without an API key which is not included.
--- punGUI.py
+++ punGUI.py
@@ -4,10 +4,22 @@ from gpt3_access import getPrompt
 
 # Root GUI object
 root = tk.Tk()
+root.title('Pun Generator')
+root.geometry("450x200")
+
+# CHeck if text is empty
+def isEmpty(text):
+    print("Empty text")
+    return str(text.get(1.0,tk.END)) == ""
 
 # Translates input text into pun version
 def generate():
-    originalText = str(input.get())
+    if isEmpty(user_input) is True:
+        user_input.insert(1.0, 'Please write something.')
+        return
+    
+    output.delete(1.0, tk.END)
+    originalText = str(user_input.get(1.0, tk.END))
     response = getPrompt(originalText)
     print(response)
     
@@ -15,22 +27,27 @@ def generate():
     outPiece = []
     outPiece = response.splitlines()
     out_text = str(outPiece[6])
-    output.insert(1, out_text[22:-1])
+    output.insert(1.0, out_text[20:-1])
 
+# Input Label
 inputLabel = Label(root, text="Enter a sentence: ")
 inputLabel.grid(row=0, column=0)
 inputLabel.pack()
 
 # Input text box
-input = Entry(root, width=50)
-input.pack()
+user_input = Text(root, width=50, height=3)
+user_input.pack()
 
 # Generate pun button
-genButton = Button(root, text="Punify", command=generate)
+genButton = Button(root, text="Punify", bg="#ff704d", command=generate, padx=10, pady=5)
 genButton.pack()
 
+# Output Label
+output_label = Label(root, text="Punny Version:")
+output_label.pack()
+
 # Output box
-output = Entry(root, width=50)
+output = Text(root, width=50, height=3)
 output.pack()
 
 
